931418026ac7babfc68665d65079b99c2888463e
Melvin W <melvinhw@me.com>
2022-11-10 11:41:23 +0000

    Initial Commit

---
+++ ChartDemo/ChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartDemoApp.swift
+//  ChartDemo
+//
+//  Created by M W on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
