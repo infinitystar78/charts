8849cc145ece7459e19cb63367f89fb62b136132
Melvin W <melvinhw@me.com>
2022-11-10 13:08:57 +0000

    Charting demo

--- ChartDemo/ContentView.swift
+++ ChartDemo/ContentView.swift
@@ -5,17 +5,32 @@
 //  Created by M W on 10/11/2022.
 //
 
+import Charts
 import SwiftUI
 
+struct Profit: Identifiable {
+    let department: String
+    let profit: Double
+    var id: String { department }
+}
+
 struct ContentView: View {
+    let data: [Profit] = [
+        Profit(department: "Production", profit: 15000),
+        Profit(department: "Marketing", profit: 8000),
+        Profit(department: "Finance", profit: 10000),
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            Chart(data) {
+                BarMark(
+                    x: .value("Department", $0.department),
+                    y: .value("Profit", $0.profit)
+                )
+            }
+            .frame(height: 250)
         }
-        .padding()
     }
 }
 
